3f4aad48261c8df997d970c8a61622bfe3f44d26
joe lee <as955133@gmail.com>
2018-06-04 10:28:53 +0800

    LINQ GroupBy 實作

--- ConsoleApp_LINQ_GroupBy、ToLookup/Program.cs
+++ ConsoleApp_LINQ_GroupBy、ToLookup/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp_LINQ_GroupBy_ToLookup
 {
@@ -6,7 +8,15 @@ namespace ConsoleApp_LINQ_GroupBy_ToLookup
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> sequences = new List<int>() { 1, 2, 4, 3, 2, 4, 6, 4, 5, 2, 2, 6, 3, 5, 7, 5 };
+
+            var qroup = sequences.GroupBy(o => o);
+
+            foreach ( var  q in qroup)
+            {
+                Console.WriteLine($" {q.Key}  count： {q.Count()}");
+            }
+            Console.ReadLine();
         }
     }
 }
